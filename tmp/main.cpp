dae050d2c25fd6183f390c964fffb218cbb6203d
Tastypotato245 <rbt2775@cau.ac.kr>
2024-11-30 14:01:33 +0900

    add: template

--- tmp/main.cpp
+++ tmp/main.cpp
@@ -1,1 +1,19 @@
 // contest
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+void Solve()
+{
+
+
+
+}
+
+int main(void)
+{
+	ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	Solve();
+	return (0);
+}
